5a6950a59b37f73979b7d90ecaa9c4a47190097d
jkarlin929 <osida11572@gmail.com>
2018-07-06 15:53:48 -0400

    render weather info to DOM

--- src/components/weather.js
+++ src/components/weather.js
@@ -4,11 +4,11 @@ class Weather extends React.Component {
   render() {
     return(
       <div>
-        {this.props.city}
-        {this.props.country}
-        {this.props.temperature}
-        {this.props.humidity}
-        {this.props.description}
+       { this.props.city && this.props.country && <p>Location: {this.props.city}, {this.props.country}</p> }
+        { this.props.temperature && <p>Temperature: {this.props.temperature}</p> }
+        { this.props.humidity && <p>Humidity: {this.props.humidity}</p> }
+        { this.props.description && <p>Condition: {this.props.description}</p> }
+        { this.props.error && <p>{this.props.error}</p> }
       </div>
     );
   }
