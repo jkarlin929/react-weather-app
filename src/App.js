718517223eea92096e712233befed4676fe78a7a
jkarlin929 <osida11572@gmail.com>
2018-07-09 16:21:51 -0400

    add css styling with bootstrap

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ class App extends Component {
     if (city && country) {
       console.log(data);
       this.setState({
-        temperature: data.main.temp,
+        temperature: Math.round((data.main.temp  * 9/5 + 32)),
         city: data.name,
         country: data.sys.country,
         humidity: data.main.humidity,
@@ -43,19 +43,32 @@ class App extends Component {
       });
     }
   }
+
   render() {
     return (
       <div>
-        <Titles />
-        <Form getWeather={this.getWeather} />
-        <Weather
-        temperature={this.state.temperature}
-        city={this.state.city}
-        country={this.state.country}
-        humidity={this.state.humidity}
-        description={this.state.description}
-        error={this.state.error}
-        />
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-xs-5 title-container">
+                  <Titles />
+                </div>
+                <div className="col-xs-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                    />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/titles.js
+++ src/components/titles.js
@@ -1,20 +1,10 @@
 import React from 'react';
 
-class Titles extends React.Component {
-  render() {
-    return(
-      <div>
-      <h1>Weather Finder!</h1>
-      <p>Find out temperature conditions in your area</p>
-      </div>
-    );
-  }
-}
-
-
-
-
-
+const Titles = () => (
+  <div>
+    <h1 className="title-container__title">Weather Finder!</h1>
+    <h3 className="title-container__subtitle">Find out temperature conditions in your area</h3>
+  </div>
+);
 
 export default Titles;
-
